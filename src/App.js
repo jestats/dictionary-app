2bbe7b3a501d79993165d288e93dad55cd8ca1fa
jestats <jestats2018@gmail.com>
2022-08-14 12:22:19 -0700

    updated img

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import openbook from "./openbook.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={openbook} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
